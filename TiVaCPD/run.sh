c95d9609a6671e56052b245324cc67144f175bfc
kopalgarg <kopalgarg24@gmail.com>
2022-09-29 22:16:09 -0400

    add simulation commands

--- TiVaCPD/run.sh
+++ TiVaCPD/run.sh
@@ -1,6 +1,10 @@
 dataset='constant_var_mean'
 data_path='./data/constant_var_mean'
 
+python3 simulate.py --constant_mean True --constant_corr False  --n_samples 1 --constant_var True --exp changing_correlation
+python3 simulate.py --constant_mean False --constant_corr True  --n_samples 20 --constant_var True --exp jumping_mean
+python3 simulate.py --constant_mean True --constant_corr True  --n_samples 20 --constant_var False --exp changing_variance
+
 python main.py --model_type 'MMDATVGL_CPD' --data_path  $data_path --exp $dataset 
 echo 'MMDATVGL_CPD done'
 python main.py --model_type 'KLCPD' --data_path $data_path --exp $dataset
