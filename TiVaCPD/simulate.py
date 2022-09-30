6b7a027ab4c058163ba91784677ed4abb0681fc9
kopalgarg <kopalgarg24@gmail.com>
2022-09-29 22:15:59 -0400

    update simulations

--- TiVaCPD/simulate.py
+++ TiVaCPD/simulate.py
@@ -5,6 +5,28 @@ import random
 from math import *
 import matplotlib.pyplot as plt
 import argparse
+from load_data import *
+import warnings
+
+def block_dataset(K, T, B=5):
+    warnings.simplefilter('ignore')
+    block_len = np.ceil(T / B)
+
+    corrs = np.zeros([B, int((K ** 2 - K) / 2 + K)])
+    
+    Y = np.zeros([T, K])
+
+    for b in np.arange(B):
+        randomCorr = random_corrmat(K)
+        print(randomCorr)
+        corrs[b, :] = mat2vec(randomCorr)
+    corrs = np.repeat(corrs, block_len, axis=0)
+    corrs = corrs[:T, :]
+
+    for t in np.arange(T):
+        Y[t, :] = np.random.multivariate_normal(mean=np.zeros([K]) + .1, cov=vec2mat(corrs[t, :]))
+
+    return Y, corrs
 
 def sim_data(n_cp=3, constant_mean = False, constant_var = False, constant_corr = False):
 
@@ -19,27 +41,27 @@ def sim_data(n_cp=3, constant_mean = False, constant_var = False, constant_corr 
 
   for i in range(n_cp):
     
-    len = int(random.randrange(100, 200))  # random length of segment
+    len = int(random.randrange(50, 100))  # random length of segment
 
     if constant_mean == False:
-      #round(random.uniform(1, 3), 2)
-      mu = [1,3,5,3,5,3,1,3,5,3,5,3,1,3,5,3,5,3][i]
+      mu = round(random.uniform(1, 3), 2)
+      #mu = [1,3,5,3,5,3,1,3,5,3,5,3,1,3,5,3,5,3][i]
     else:
+      
       mu = 0
     
     if constant_var == False:
-      #round(random.uniform(.1, .7),2)
-      sigma = [.1,.3,.5,.3,.5,.3,.1,.3,.5,.3,.5,.3,.1,.3,.5,.3,.5,.3][i]
+      #sigma = round(random.uniform(.1, .7),2)
+      sigma = [.1,.8,.1,.8,.1,.8,.1,.8,.5,.3,.5,.3,.1,.3,.5,.3,.5,.3][i]
     else:
-      sigma = 0.5
+      sigma = 0.1
     
     if constant_corr == False:
-      rho = int(random.randrange(-1, 2)) #round(random.uniform(-1,1),2)
+      rho = [1, -1, 0, 1, -1, 0, 1, -1][i] #int(random.randrange(-1, 2)) #round(random.uniform(-1,1),2)
 
     else:
       rho = 0
 
-    
     mean = np.concatenate((mean, np.repeat(mu, len)))
     var = np.concatenate((var, np.repeat(sigma, len)))
     cor = np.concatenate((cor, np.repeat(rho, len)))
@@ -65,23 +87,23 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='change point detection')
     parser.add_argument('--path', type=str, default='./data')
-    parser.add_argument('--constant_mean', type = bool, default = False)
-    parser.add_argument('--constant_var', type = bool, default = False)
-    parser.add_argument('--constant_corr', type = bool, default = False)
+    parser.add_argument('--constant_mean', default = 'False')
+    parser.add_argument('--constant_var', default = 'False')
+    parser.add_argument('--constant_corr', default = 'False')
     parser.add_argument('--exp', default = '0')
     parser.add_argument('--num_cp', type = int, default = 3)
     parser.add_argument('--n_samples', type = int, default = 20)
 
     args = parser.parse_args()
-    print(args.constant_mean)
+
     if not os.path.exists(os.path.join(args.path, args.exp)): 
         os.mkdir(os.path.join(args.path, args.exp))
     data_path = os.path.join(args.path, args.exp)
 
     for i in range(args.n_samples):
-      X, mean, var, cor = sim_data(n_cp = args.num_cp, constant_mean = args.constant_mean, 
-                                                    constant_var = args.constant_var, 
-                                                    constant_corr = args.constant_corr)
+      X, mean, var, cor = sim_data(n_cp = args.num_cp, constant_mean = eval(args.constant_mean), 
+                                                    constant_var = eval(args.constant_var), 
+                                                    constant_corr = eval(args.constant_corr))
 
       data_path = os.path.join(args.path, args.exp)
 
